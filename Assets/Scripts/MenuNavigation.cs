dce7c75636d8c4c95195569db7a7c5dbbf2364df
Abdullah <abdullahijaz04@outlook.com>
2024-02-22 18:52:26 +0000

    Panel follows user; close button to quit app; clicking help should open website; restyling

--- Assets/Scripts/MenuNavigation.cs
+++ Assets/Scripts/MenuNavigation.cs
@@ -9,4 +9,16 @@ public class MenuNavigation : MonoBehaviour
     {
         SceneManager.LoadScene("ExperimentSelection");
     }
+    public void goToSettings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+    public void openDocumentation()
+    {
+        Application.OpenURL("https://github.com/LabVerse/LabVerse");
+    }
+    public void quitApp()
+    {
+        Application.Quit();
+    }
 }
