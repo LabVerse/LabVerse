51e1444c0eaded82683efaad10a6f452745a3740
AlexTClarke <119004428+AlexTClarke@users.noreply.github.com>
2024-02-24 17:58:12 +0000

    Add Testing for change scene button

--- Assets/Scripts/SceneLogic.cs
+++ Assets/Scripts/SceneLogic.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Controls Scene changes.
+/// </summary>
 public class SceneLogic : MonoBehaviour
 {
+    /// <summary>
+    /// Changes Scene.
+    /// </summary>
     public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+
     }
 }
