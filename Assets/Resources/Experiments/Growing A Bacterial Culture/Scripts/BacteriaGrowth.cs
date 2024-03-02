9a41a9dde4e0b5151dc7e9a825bedcc478476c8b
Sharkywrecks <104490816+Sharkywrecks@users.noreply.github.com>
2024-03-02 00:12:20 +0000

    Fix: Deleted testing container and adapted bacteria scripts
    Bacteria scripts where affected by parent scale, so had to adjust code to handle this.

--- Assets/Resources/Experiments/Growing A Bacterial Culture/Scripts/BacteriaGrowth.cs
+++ Assets/Resources/Experiments/Growing A Bacterial Culture/Scripts/BacteriaGrowth.cs
@@ -5,23 +5,25 @@ using UnityEngine;
 
 public class BacteriaGrowth : MonoBehaviour
 {
-    public float growthRate = 0.01f;
+    public float radiusOfBacteria;
+    public float growthRate = 0.005f;
     public float timePerGeneration = 1.0f;
     public float maxBacteriaSize = 1.0f;
     public float radius = 0;
-    Transform bacteriaTransform;
     double timeSinceLastGeneration = 0.0;
     Bounds meshBounds;
-    float distanceFromCenter;
+    [SerializeField] float distanceFromCenter;
+    public Vector3 parentScale;
+    [SerializeField] Vector3 localBounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        bacteriaTransform = GetComponent<Transform>();
         timePerGeneration += Random.Range(-0.5f, 0.5f);
         maxBacteriaSize -= Random.Range(0.0f, maxBacteriaSize * 0.5f);
         meshBounds = GetComponent<MeshFilter>().mesh.bounds;
-        distanceFromCenter = Mathf.Pow(transform.localPosition.x, 2) + Mathf.Pow(transform.localPosition.y, 2) + Mathf.Pow(transform.localPosition.z, 2); //Sqrt is expensive so don't use
+        distanceFromCenter = Mathf.Pow(transform.localPosition.x * parentScale.x, 2) + Mathf.Pow(transform.localPosition.y * parentScale.y, 2) + Mathf.Pow(transform.localPosition.z * parentScale.z, 2); //Sqrt is expensive so don't use
+        parentScale = GetComponentInParent<ContainerScale>().scale;
     }
 
     // Update is called once per frame
@@ -30,13 +32,13 @@ public class BacteriaGrowth : MonoBehaviour
         if(timeSinceLastGeneration >= timePerGeneration)
         {
             timeSinceLastGeneration = 0.0;
-            bacteriaTransform.localScale += new Vector3(growthRate, 0, growthRate);
+            transform.localScale += new Vector3(growthRate / parentScale.x, 0, growthRate / parentScale.z);
         }
         timeSinceLastGeneration += Time.deltaTime;
 
-        Vector3 localBounds = new Vector3(meshBounds.max.x * transform.localScale.x, meshBounds.max.y * transform.localScale.y, meshBounds.max.z * transform.localScale.z);
-
+        localBounds = new Vector3(meshBounds.max.x * parentScale.x * transform.localScale.x, meshBounds.max.y * parentScale.y * transform.localScale.y, meshBounds.max.z * parentScale.z * transform.localScale.z);
         //If bacteria reached max size or container bounds, stop growing
+
         if (MaxSizeReached(localBounds) || ReachedContainerBounds(localBounds))
         {
             this.enabled = false;
@@ -44,12 +46,13 @@ public class BacteriaGrowth : MonoBehaviour
     }
     private bool MaxSizeReached(Vector3 localBounds)
     {
-        return Mathf.Max(localBounds.x, localBounds.y, localBounds.z) >= maxBacteriaSize;
+        return Mathf.Max(localBounds.x, localBounds.y, localBounds.z ) >= maxBacteriaSize;
     }
     private bool ReachedContainerBounds(Vector3 localBounds)
     {
-        float radiusOfBacteria = Mathf.Pow(localBounds.x, 2) + Mathf.Pow(localBounds.y,2) + Mathf.Pow(localBounds.z, 2);
+        radiusOfBacteria = Mathf.Pow(localBounds.x, 2)+ Mathf.Pow(localBounds.y,2) + Mathf.Pow(localBounds.z, 2);
         float radiusSquared = Mathf.Pow(radius, 2);
+        Debug.Log((distanceFromCenter + radiusOfBacteria)+" VS "+ radiusSquared);
         return distanceFromCenter + radiusOfBacteria >= radiusSquared;
     }
 }
