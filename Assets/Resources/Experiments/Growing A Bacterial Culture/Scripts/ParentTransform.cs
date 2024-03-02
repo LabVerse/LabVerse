5a843522ea316c7967afb7bb6c6c5c615befa048
Sharkywrecks <104490816+Sharkywrecks@users.noreply.github.com>
2024-03-02 22:14:55 +0000

    Feat: Experiment
    Specified trigger interactions between experiment items and corresponding logic.

--- Assets/Resources/Experiments/Growing A Bacterial Culture/Scripts/ParentTransform.cs
+++ Assets/Resources/Experiments/Growing A Bacterial Culture/Scripts/ParentTransform.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContainerScale : MonoBehaviour
+public class ParentTransform : MonoBehaviour
 {
+    public Vector3 rotation;
     public Vector3 scale;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        rotation = transform.localEulerAngles;
         scale = transform.lossyScale;
     }
 }
