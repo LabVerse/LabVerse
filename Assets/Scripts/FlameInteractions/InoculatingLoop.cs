51d46b1371db8e094adfd7456d8bfb259fa8cb10
KaiMattinson <119963354+KaiMattinson@users.noreply.github.com>
2024-03-06 19:45:27 +0000

    feat: Flame interaction additions (#26)
    ## Briefly describe your changes
    - Fixes to the branch based on previous PR
    - Small changes to the flame models
    
    ## Complete the below before merging
    - [x] I have performed a self-review of my code.
    - [x] If this is a feature branch, I have added thorough unit or
    implementation tests.
    - [x] I have ensured that all existing tests pass.
    - [ ] My PR has been approved by at least one person that I've requested
    to review.

---
+++ Assets/Scripts/FlameInteractions/InoculatingLoop.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+/// <summary>
+/// Controls the inoculating loop prefab 'picking up' a sample of whatever metal it collides with
+/// </summary>
+public class InoculatingLoop : MonoBehaviour
+{
+    [SerializeField] private GameObject[] metalSamples;
+    private bool m_isColliding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_isColliding = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_isColliding=false;
+    }
+
+    ///<summary>
+    /// When the inoculating loop collides with a metal source, remove any other child metals and add the current one
+    ///<\summary>
+    public void OnTriggerEnter(Collider other)
+    {
+        // Prevent detecting multiple collisions per update
+        // NB: Possibly better ways to do this than dropping all other collisions,
+        //     but right now performance hit is too great to leave, so improve in future
+        if (m_isColliding) { return; }
+        m_isColliding=true;
+
+        if (other.gameObject.CompareTag("Metal"))
+        {
+            int i = 0;
+
+            // Remove existing metal sample
+            foreach (Transform child in transform)
+            {
+                // Determine which metal was on the inoculation loop
+                // Use 'ObjectName(Clone)' as this is how children will be named
+                // NB: Consider improvements for this code in future
+                if (child.gameObject.name == "CopperSample(Clone)") { i = 1; } 
+                else if (child.gameObject.name == "LithiumSample(Clone)") { i = 2; }
+                else if (child.gameObject.name == "IronSample(Clone)") { i = 3; }
+                else { continue; }
+
+                // Remove the metal from the inoculating loop
+                Destroy(child.gameObject);
+                
+                //Fail the given stage, as the metal was put aside
+                StageManager.instance.FinishStage(i, false);
+            }
+
+            // Determine which metal should be added to the loop
+            if (other.gameObject.name == "CopperSource") { i = 1; }
+            else if (other.gameObject.name == "LithiumSource") { i = 2; }
+            else if(other.gameObject.name == "IronSource") { i = 3; }
+
+            // Spawn the metal on the loop
+            GameObject newChild = Instantiate(metalSamples[i-1], new Vector3(0, 0, 0), Quaternion.identity);
+            newChild.transform.parent = transform;
+
+            Vector3 colliderPos = new Vector3(0.00465f, 0, 0);
+            newChild.transform.localPosition = colliderPos;
+
+            // Start the correct stage of the experiment
+            StageManager.instance.EnterStage(i);
+        }
+    }
+}
---
+++ Assets/Scripts/FlameInteractions/MetalFlameBehaviour.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+/// <summary>
+/// Controls the flames burning on metal attached to the inoculating loop
+/// </summary>
+public class MetalFlameBehaviour : MonoBehaviour
+{
+    [SerializeField] private int stageIndex;
+    private GameObject m_flame;
+    private bool m_burning;
+
+    // Depending on element, set specific properties for flame colour, burn time, etc. here:
+    [SerializeField] private float countdown = 8f;
+    // Other properties here:
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get references to key child components
+        m_flame = transform.GetChild(0).gameObject;
+
+        //set default values for attributes
+        m_burning = false;
+        m_flame.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_burning) { BurnTick(); }    
+    }
+
+    /// <summary>
+    /// Register one frame of the metal burning, if countdown hits zero destroy the gameobject
+    /// </summary>
+    private void BurnTick()
+    {
+        // Decrement countdown to zero
+        countdown -= (1 * Time.deltaTime);
+        if (countdown <= 0)
+        {
+            // Once zero hit, metal has 'burned up' so delete it
+            Destroy(transform.gameObject);
+
+            // Call end of stage in stage manager
+            StageManager.instance.FinishStage(stageIndex, true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if collision was with a bunsen burner's flame
+        if (other.transform.gameObject.name == "BunsenFlame")
+        {
+            // Check if the bunsen burner's flame is lit
+            BunsenBurnerFlames bunsenScript = other.transform.GetComponent<BunsenBurnerFlames>();
+            if (bunsenScript.IsLit())
+            {
+                m_burning = true;
+                m_flame.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // Check if collision was with a bunsen burner's flame
+        if (other.transform.gameObject.name == "BunsenFlame")
+        {
+            // Metal has left the flame, so stop burning
+            m_burning = false;
+            m_flame.SetActive(false);
+        }
+    }
+}
