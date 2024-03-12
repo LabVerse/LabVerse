f5f876d6bacd9ec70bce31fee3a1cd728af60064
KaiMattinson <119963354+KaiMattinson@users.noreply.github.com>
2024-03-12 22:42:32 +0000

    fix: bug fixes

--- Assets/Scripts/RadiationInteractions/GeigerScript.cs
+++ Assets/Scripts/RadiationInteractions/GeigerScript.cs
@@ -11,36 +11,29 @@ public class GeigerScript : MonoBehaviour
     [SerializeField]
     private int m_geigerValue; // Min = 0, Max = 1000
 
-    private int prev;
-
 
     // Start is called before the first frame update
     void Start()
     {
         geigerScreen = GameObject.Find("GeigerCanvas/Text (TMP)").GetComponent<TMP_Text>();
-        m_geigerValue = 0;
-        prev = 18; // default CPM, standard room radioactivity
+        m_geigerValue = 18; //Default room CPM
     }
 
     // FixedUpdate is called every 0.2s
     void FixedUpdate()
     {
-        // Update value on gameobject display, whenever geiger value changes
-        if (m_geigerValue != prev)
-        {
-            prev = m_geigerValue;
-            UpdateDisplay();
-        }
+        // Update value on gameobject display
+        UpdateDisplay();
 
         // Should geiger value decrement, or be set to zero, after each update?
-        
+        //m_geigerValue--;
     }
     public void ParticleHit(RadiationParticles.RADIATION_TYPES particle)
     {
-        // Radioactive sample will call this on every collision
-
+        // Geiger Tube will call this on every collision
         // Adjust the value of the geiger counter as necessary
-
+        m_geigerValue++;
+        if (m_geigerValue > 1000) { m_geigerValue = 1000; } //cap at 1k cpm
 
     }
 
